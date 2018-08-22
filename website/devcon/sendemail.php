da07d2fc850fc479c04047c252f2885666d48b3d
k9uma <kuma.musenge@gmail.com>
2018-08-22 10:06:18 +0200

    First Commit
    Created Files and Folders

---
+++ website/devcon/sendemail.php
@@ -0,0 +1,73 @@
+<?php
+   
+    session_cache_limiter( 'nocache' );
+    header( 'Expires: ' . gmdate( 'r', 0 ) );
+    header( 'Content-type: application/json' );
+
+
+    $to         = 'mukuma.m@agoracode.community'; //put your email here
+
+    $email_template = 'simple.html';
+
+    $subject    = "SUBJECT";
+    $email      = strip_tags($_POST['email']);
+    //$phone      = "";
+    //$name       = strip_tags($_POST['name']);
+    //$message    = nl2br( htmlspecialchars($_POST['message'], ENT_QUOTES) );
+    $result     = array();
+
+/*
+    if(empty($name)){
+
+        $result = array( 'response' => 'error', 'empty'=>'name', 'message'=>'<strong>Error!</strong> Name is empty.' );
+        echo json_encode($result );
+        die;
+    } 
+*/
+    if(empty($email)){
+
+        $result = array( 'response' => 'error', 'empty'=>'email', 'message'=>'<strong>Error!</strong> Email is empty.' );
+        echo json_encode($result );
+        die;
+    } 
+/*
+    if(empty($message)){
+
+         $result = array( 'response' => 'error', 'empty'=>'message', 'message'=>'<strong>Error!</strong> Message body is empty.' );
+         echo json_encode($result );
+         die;
+    }
+    
+*/
+
+    $headers  = "From: ". $email . '>' . "\r\n";
+    $headers .= "Reply-To: ". $email . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+
+
+    $templateTags =  array(
+        '{{subject}}' => $subject,
+        '{{email}}'=>$email,
+        //'{{message}}'=>$message,
+        //'{{name}}'=>$name,
+        //'{{phone}}'=>$phone
+        );
+
+
+    //$templateContents = file_get_contents( dirname(__FILE__) . '/email-templates/'.$email_template);
+
+    //$contents =  strtr($templateContents, $templateTags);
+
+    $contents = $email;
+    if ( mail( $to, $subject, $contents, $headers ) ) {
+        $result = array( 'response' => 'success', 'message'=>'<strong>Success!</strong> Mail Sent.' );
+        $_SESSION['response'] = "<strong>Success!</strong> Added to Mailing List.";
+    } else {
+        $result = array( 'response' => 'error', 'message'=>'<strong>Error!</strong> Cann\'t Send Mail.'  );
+        $_SESSION['response'] = "<strong>Failed!</strong> Sorry Please Try Again.";
+    }
+
+    
+    header("Location: http://devcon.agoracode.community/#section-venue");
+    die();
